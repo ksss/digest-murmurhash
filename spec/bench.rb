82e263282aed4086624e66d21eeea8cedf6fdc72
ksss <co000ri@gmail.com>
2013-12-08 11:36:09 +0900

    add spec that total second rate and confrict count.

--- spec/bench.rb
+++ spec/bench.rb
@@ -3,8 +3,23 @@
 lib = File.expand_path('../../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
+require 'digest/stringbuffer'
 require 'digest/murmurhash'
 require 'benchmark'
+
+class Prime37 < Digest::StringBuffer
+  def initialize
+    @prime = 37
+  end
+
+  def finish
+    result = 0
+    buffer.unpack("C*").each do |c|
+      result += (c * @prime)
+    end
+    [result & 0xffffffff].pack("N")
+  end
+end
 
 class Integer
   def to_32
@@ -47,22 +62,79 @@ def murmur_hash str
   h
 end
 
+@rands = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split(//)
 def rand_str
   rand = "";
-  File.open("/dev/urandom").read(20).each_byte{|x| rand << sprintf("%02x",x)}
+  20.times {
+    rand << @rands[rand(62)]
+  }
   rand
 end
 
-s = rand_str
-p murmur_hash(s)
-p Digest::MurmurHash1.rawdigest(s)
+n = 100000
+times_enum = n.times
 
+a = Array.new(n, 0)
+n.times do |i|
+  a[i] = rand_str
+end
+
+c = Struct.new "Cases",
+               :name,
+               :func
+cases = [
+  c.new("pureRuby", proc{|x| murmur_hash x }),
+  c.new("Prime37", proc{|x| Prime37.digest x }),
+  c.new("MurmurHash1", proc{|x| Digest::MurmurHash1.rawdigest x }),
+  c.new("MurmurHash2", proc{|x| Digest::MurmurHash2.rawdigest x }),
+  c.new("MurmurHash2A", proc{|x| Digest::MurmurHash2A.rawdigest x }),
+]
+
+reals = {}
+confrict = {}
+confricts = {}
+
+puts "### condition"
+puts
+puts "    RUBY_VERSION = #{RUBY_VERSION}"
+puts "    count = #{n}"
+puts
+puts "### benchmark"
+puts
+puts "```"
 Benchmark.bm do |x|
-  n = 10000
-  x.report {n.times{ |i|
-    murmur_hash(s)
-  }}
-  x.report {n.times{ |i|
-    Digest::MurmurHash1.rawdigest(s)
-  }}
+  cases.each do |c|
+    z = x.report c.name do
+      times_enum.each do |i|
+        c.func.call(a[i])
+      end
+    end
+
+    confrict.clear
+    times_enum.each do |i|
+      rethash = c.func.call(a[i])
+      if confrict[rethash].nil?
+        confrict[rethash] = 0
+      else 
+        confrict[rethash] += 1
+      end
+    end
+    reals[c.name] = z.real
+    confricts[c.name] = confrict.count{|hash, count| 0 < count}
+  end
+end
+puts "```"
+
+puts
+puts "### real second rate (pureRuby/)"
+puts
+reals.each do |name, real|
+  puts "    " + (reals["pureRuby"] / real).to_s + "/" + name
+end
+
+puts
+puts "### confrict count (/#{n})"
+puts
+confricts.each do |name, count|
+  puts "    #{name}: #{count}"
 end
